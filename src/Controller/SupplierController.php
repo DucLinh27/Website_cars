4c58d370b54d68ae57b3dfffbbc48661e0bf160a
HoangANhne <anhnthgcd201650@fpt.edu.vn>
2022-06-23 16:30:27 +0700

    Detail of Supplier

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -12,13 +12,24 @@ class SupplierController extends AbstractController
     /**
      * @Route("/supplier", name="supplier_list")
      */
-    public function listAction()
+    public function listAction() : Response
+    {
+        $suppliers = $this->getDoctrine()->getRepository(Supplier::class)->findAll();
+        return $this->render('supplier/index.html.twig', [
+            'suppliers' => $suppliers
+        ]);
+    }
+    /**
+     * @Route("/supplier/details/{id}", name="supplier_details")
+     */
+    function detailsAction($id) : Response
     {
         $supplier = $this->getDoctrine()
             ->getRepository(Supplier::class)
-            ->findAll();
-        return $this->render('supplier/index.html.twig', [
-            'suppliers' => $supplier
+            ->find($id);
+
+        return $this->render('supplier/details.html.twig', [
+            'supplier' => $supplier
         ]);
     }
 }
