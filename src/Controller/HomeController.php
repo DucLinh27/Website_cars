b41fca92401dfa98c0f78b63d4f7f0a83fd6f5f0
thangnetroi <thangnhgcd201443@fpt.edu.vn>
2022-06-23 14:22:48 +0700

    Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/home", name="home_list")
      */
     public function index(): Response
     {
+        $homes = $this->getDoctrine()
+            ->getRepository(Car::class)
+            ->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'home' => $homes,
         ]);
     }
+
+
 }
