cd5335bc7b1074c3860391ec0379c4b5884a34b3
HoangANhne <anhnthgcd201650@fpt.edu.vn>
2022-06-24 10:41:04 +0700

    add function creat supplier

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Supplier;
+use App\Form\SupplierType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -49,4 +51,46 @@ class SupplierController extends AbstractController
 
         return $this->redirectToRoute('supplier_list');
     }
+    /**
+     * @Route("/supplier/create", name="supplier_create", methods={"GET","POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $supplier = new Supplier();
+        $form = $this->createForm(SupplierType::class, $supplier);
+
+        if ($this->saveChanges($form, $request, $supplier)) {
+            $this->addFlash(
+                'notice',
+                'Added Successful'
+            );
+
+            return $this->redirectToRoute('supplier_list');
+        }
+
+        return $this->render('supplier/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function saveChanges($form, $request, $supplier): bool
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+//            $supplier->setSupplierID($request->request->get('supplier')['supplierID']);
+//            $supplier->setSuppliername($request->request->get('supplier')['Suppliername']);
+//            $supplier->setPhonenumber($request->request->get('supplier')['Phonenumber']);
+//            $supplier->setAddress($request->request->get('supplier')['Address']);
+//            $car->setPriority($request->request->get('car')['priority']);
+//            $car->setDueDate(\DateTime::createFromFormat('Y-m-d', $request->request->get('todo')['due_date']));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($supplier);
+            $em->flush();
+
+            return true;
+        }
+        return false;
+    }
 }
+
--- src/Entity/Supplier.php
+++ src/Entity/Supplier.php
@@ -81,3 +81,4 @@ class Supplier
         return $this;
     }
 }
+
